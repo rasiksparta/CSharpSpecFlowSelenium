57b25687a53db0f3465826033b8c17b1a35a2c2e
Rasik Rana <RRana@spartaglobal.com>
2019-10-14 13:42:38 +0100

    version 1.0

---
+++ CSharpSpeFlowSeleniumTwo/BBCLogin.feature
@@ -0,0 +1,12 @@
+﻿Feature: BBCLogin
+	In order to login into my account
+	As a user
+	I want to be able to see my account page
+
+@BBCLogin
+Scenario: Invalid password
+	Given I am on the login page
+	And I have entered a valid username
+	And I have entered a invalid password
+	When I press login 
+	Then I should see the appropriate error
---
+++ CSharpSpeFlowSeleniumTwo/BBCLoginSteps.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace CSharpSpecFlowSelenium
+{
+    [Binding]
+    public class BBCLoginSteps
+    {
+        // web driver
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Given(@"I am on the login page")]
+        public void GivenIAmOnTheLoginPage()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://BBC.co.uk/");
+            IWebElement loginButton = driver.FindElement(By.Id("idcta-link"));
+            loginButton.Click();
+        }
+
+        [Given(@"I have entered a valid username")]
+        public void GivenIHaveEnteredAValidUsername()
+        {
+            IWebElement userNameField = driver.FindElement(By.Id("user-identifier-input"));
+            userNameField.SendKeys("edbox52@gmail.com");
+        }
+
+        [Given(@"I have entered a invalid password")]
+        public void GivenIHaveEnteredAInvalidPassword()
+        {
+            IWebElement passwordField = driver.FindElement(By.Id("password-input"));
+            passwordField.SendKeys("1234566678");
+        }
+
+        [When(@"I press login")]
+        public void WhenIPressLogin()
+        {
+            IWebElement signInButton = driver.FindElement(By.Id("submit-button"));
+            signInButton.Click();
+        }
+
+        [Then(@"I should see the appropriate error")]
+        public void ThenIShouldSeeTheAppropriateError()
+        {
+            IWebElement error = driver.FindElement(By.Id("form-message-password"));
+            Assert.AreEqual("Sorry, that password isn't valid. Please include a letter.", error.Text);
+        }
+
+        [AfterScenario]
+        public void cleanUp()
+        {
+            driver.Close();
+        }
+    }
+}
